ee181f8a9f90828337fc094aeb156d0d91e15070
GenkiSugiyama <sgym0318marcos@gmail.com>
2024-11-12 00:38:06 +0900

    struct receiver

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 // const宣言は定数となる
@@ -22,6 +23,12 @@ var (
 	b string
 	c bool
 )
+
+// struct 構造体
+type Task struct {
+	Title    string
+	Estimate int
+}
 
 func main() {
 	// i := 2
@@ -80,68 +87,108 @@ func main() {
 	// }
 	// fmt.Println(result)
 
-	// slice
-	//　配列の定義 [要素数]要素の型 で初期化 intの配列は要素数分の 0 が初期値に入る
-	var a1 [3]int
-	var a2 = [3]int{1, 2, 3}
-	a3 := [...]int{1, 2} // [...] と記述すると代入時の要素数を自動カウントしてくれる
+	// // slice
+	// //　配列の定義 [要素数]要素の型 で初期化 intの配列は要素数分の 0 が初期値に入る
+	// var a1 [3]int
+	// var a2 = [3]int{1, 2, 3}
+	// a3 := [...]int{1, 2} // [...] と記述すると代入時の要素数を自動カウントしてくれる
 
-	fmt.Printf("%v, %v, %v\n", a1, a2, a3)
-	fmt.Printf("%v, %v\n", len(a3), cap(a3)) // cap()は中身の要素の型によって出力結果が異なる、配列の場合は要素数でlen()と同じ
-	fmt.Printf("%T, %T\n", a2, a3)           // 配列の型は中身の型と要素数によって決まる、同じ数値配列でも要素数が異なると別々の型となる
+	// fmt.Printf("%v, %v, %v\n", a1, a2, a3)
+	// fmt.Printf("%v, %v\n", len(a3), cap(a3)) // cap()は中身の要素の型によって出力結果が異なる、配列の場合は要素数でlen()と同じ
+	// fmt.Printf("%T, %T\n", a2, a3)           // 配列の型は中身の型と要素数によって決まる、同じ数値配列でも要素数が異なると別々の型となる
 
-	// slice 配列は一度宣言したら要素数の追加等できない、あとから要素を追加したりする場合は slice型を使う
-	var s1 []int // []の中身空で slice の宣言
-	s2 := []int{}
-	fmt.Printf("s1: %[1]T %[1]v %v %v\n", s1, len(s1), cap(s1))
-	fmt.Printf("s2: %[1]T %[1]v %v %v\n", s2, len(s2), cap(s2))
-	fmt.Println(s1 == nil)   // var で宣言された slice は nil 扱いだが
-	fmt.Println(s2 == nil)   // := で宣言された slice は nil ではない
-	s1 = append(s1, 1, 2, 3) // append(slice変数, 要素1, 要素2, ~) で既存slice に要素を追加する
-	fmt.Printf("s1: %[1]T %[1]v %v %v\n", s1, len(s1), cap(s1))
-	s3 := []int{4, 5, 6}
-	s1 = append(s1, s3...) // sliceに別のsliceを追加するときは append(追加元slice, 取り込まれるslice...) のように第二引数の slice に ...（スリードット）を繋げる
-	fmt.Printf("s1: %[1]T %[1]v %v %v\n", s1, len(s1), cap(s1))
+	// // slice 配列は一度宣言したら要素数の追加等できない、あとから要素を追加したりする場合は slice型を使う
+	// var s1 []int // []の中身空で slice の宣言
+	// s2 := []int{}
+	// fmt.Printf("s1: %[1]T %[1]v %v %v\n", s1, len(s1), cap(s1))
+	// fmt.Printf("s2: %[1]T %[1]v %v %v\n", s2, len(s2), cap(s2))
+	// fmt.Println(s1 == nil)   // var で宣言された slice は nil 扱いだが
+	// fmt.Println(s2 == nil)   // := で宣言された slice は nil ではない
+	// s1 = append(s1, 1, 2, 3) // append(slice変数, 要素1, 要素2, ~) で既存slice に要素を追加する
+	// fmt.Printf("s1: %[1]T %[1]v %v %v\n", s1, len(s1), cap(s1))
+	// s3 := []int{4, 5, 6}
+	// s1 = append(s1, s3...) // sliceに別のsliceを追加するときは append(追加元slice, 取り込まれるslice...) のように第二引数の slice に ...（スリードット）を繋げる
+	// fmt.Printf("s1: %[1]T %[1]v %v %v\n", s1, len(s1), cap(s1))
 
-	s4 := make([]int, 0, 2) // makeメソッドを使用してsliceを宣言する make(型情報, 要素数, 容量)
-	fmt.Printf("s4: %[1]T %[1]v %v %v\n", s4, len(s4), cap(s4))
-	s4 = append(s4, 1, 2, 3, 4)
-	fmt.Printf("s4: %[1]T %[1]v %v %v\n", s4, len(s4), cap(s4))
+	// s4 := make([]int, 0, 2) // makeメソッドを使用してsliceを宣言する make(型情報, 要素数, 容量)
+	// fmt.Printf("s4: %[1]T %[1]v %v %v\n", s4, len(s4), cap(s4))
+	// s4 = append(s4, 1, 2, 3, 4)
+	// fmt.Printf("s4: %[1]T %[1]v %v %v\n", s4, len(s4), cap(s4))
 
-	s5 := make([]int, 4, 6)
-	fmt.Printf("s5: %[1]T %[1]v %v %v\n", s5, len(s5), cap(s5))
-	fmt.Printf("%v\n", s5[1:3])
-	s6 := s5[1:3] // slice[index:index] の指定で 左のindex<= 対象 < 右のindex の範囲の値を抽出できる 今回だとindex:1番目以上3番未満なので、要素の2番目、3番目の2つを抽出
-	fmt.Printf("s6: %[1]T %[1]v %v %v\n", s6, len(s6), cap(s6))
-	s6[1] = 10
-	fmt.Printf("s5: %[1]T %[1]v %v %v\n", s5, len(s5), cap(s5))
-	fmt.Printf("s6: %[1]T %[1]v %v %v\n", s6, len(s6), cap(s6))
-	// sliceの一部を別のsliceに代入するとそのslice同士はメモリを共有することになる
-	// 新しく宣言したsliceに値を追加すると、元のsliceの中身も書き換えられる
-	s6 = append(s6, 20)
-	fmt.Printf("s5: %[1]T %[1]v %v %v\n", s5, len(s5), cap(s5)) // s6に要素を追加するとs5の4番目（index 4）の値も書き換わる
-	fmt.Printf("s6: %[1]T %[1]v %v %v\n", s6, len(s6), cap(s6))
+	// s5 := make([]int, 4, 6)
+	// fmt.Printf("s5: %[1]T %[1]v %v %v\n", s5, len(s5), cap(s5))
+	// fmt.Printf("%v\n", s5[1:3])
+	// s6 := s5[1:3] // slice[index:index] の指定で 左のindex<= 対象 < 右のindex の範囲の値を抽出できる 今回だとindex:1番目以上3番未満なので、要素の2番目、3番目の2つを抽出
+	// fmt.Printf("s6: %[1]T %[1]v %v %v\n", s6, len(s6), cap(s6))
+	// s6[1] = 10
+	// fmt.Printf("s5: %[1]T %[1]v %v %v\n", s5, len(s5), cap(s5))
+	// fmt.Printf("s6: %[1]T %[1]v %v %v\n", s6, len(s6), cap(s6))
+	// // sliceの一部を別のsliceに代入するとそのslice同士はメモリを共有することになる
+	// // 新しく宣言したsliceに値を追加すると、元のsliceの中身も書き換えられる
+	// s6 = append(s6, 20)
+	// fmt.Printf("s5: %[1]T %[1]v %v %v\n", s5, len(s5), cap(s5)) // s6に要素を追加するとs5の4番目（index 4）の値も書き換わる
+	// fmt.Printf("s6: %[1]T %[1]v %v %v\n", s6, len(s6), cap(s6))
 
-	// map: key-valueの組み合わせ、C#のDictionaryみたいな
-	var m1 map[string]int  // map[keyの型]valueの型 でmapの宣言
-	m2 := map[string]int{} // := での宣言時は valueの型の後ろに空の {} をつける
-	fmt.Println(m1 == nil) // slice 同様 var で宣言された map は nil だが
-	fmt.Println(m2 == nil) // := で宣言されたものは nil とならない
+	// // map: key-valueの組み合わせ、C#のDictionaryみたいな
+	// var m1 map[string]int  // map[keyの型]valueの型 でmapの宣言
+	// m2 := map[string]int{} // := での宣言時は valueの型の後ろに空の {} をつける
+	// fmt.Println(m1 == nil) // slice 同様 var で宣言された map は nil だが
+	// fmt.Println(m2 == nil) // := で宣言されたものは nil とならない
 
-	m2["A"] = 10 // key とそれに対応するvalueのセット
-	m2["B"] = 20
-	m2["C"] = 30
-	fmt.Printf("m2: %[1]T %[1]v %v %v\n", m2, len(m2), m2["A"])
-	delete(m2, "B")
-	fmt.Printf("m2: %[1]T %[1]v %v %v\n", m2, len(m2), m2["B"]) // 存在しないkeyを呼び出すと0を返すが存在するkeyの値か、keyが存在せず返した値かは見分けられる
+	// m2["A"] = 10 // key とそれに対応するvalueのセット
+	// m2["B"] = 20
+	// m2["C"] = 30
+	// fmt.Printf("m2: %[1]T %[1]v %v %v\n", m2, len(m2), m2["A"])
+	// delete(m2, "B")
+	// fmt.Printf("m2: %[1]T %[1]v %v %v\n", m2, len(m2), m2["B"]) // 存在しないkeyを呼び出すと0を返すが存在するkeyの値か、keyが存在せず返した値かは見分けられる
 
-	value, exist := m2["A"] // keyを指定して値を取り出す時の第二戻り値にそのkeyが存在するかどうかを判定するbool値がある
-	fmt.Printf("%v, %v\n", value, exist)
-	value, exist = m2["B"] // bool値の値がtrue なら存在する値を取り出した、falseならkeyが存在せず0を返した、と判定できる
-	fmt.Printf("%v, %v\n", value, exist)
+	// value, exist := m2["A"] // keyを指定して値を取り出す時の第二戻り値にそのkeyが存在するかどうかを判定するbool値がある
+	// fmt.Printf("%v, %v\n", value, exist)
+	// value, exist = m2["B"] // bool値の値がtrue なら存在する値を取り出した、falseならkeyが存在せず0を返した、と判定できる
+	// fmt.Printf("%v, %v\n", value, exist)
 
-	for k, v := range m2 {
-		fmt.Printf("%v, %v\n", k, v)
-	} // for ～ range map {} で map 内の要素をすべて取り出す、map はハッシュマップとなっているので順番は保障されていない
+	// for k, v := range m2 {
+	// 	fmt.Printf("%v, %v\n", k, v)
+	// } // for ～ range map {} で map 内の要素をすべて取り出す、map はハッシュマップとなっているので順番は保障されていない
 
+	// struct型の実体化
+	task1 := Task{
+		Title:    "Learn Golang",
+		Estimate: 3,
+	}
+	task1.Title = "Learning Go"
+	fmt.Printf("%[1]T %+[1]v, %v\n", task1, task1.Title)
+
+	var task2 Task = task1
+	task2.Title = "new"
+	fmt.Printf("task1: %v task2: %v\n", task1.Title, task2.Title)
+
+	task1p := &Task{ // 初期化時の型の先頭に & をつけてポインタを取得
+		Title:    "Learn concurrency",
+		Estimate: 2,
+	}
+	fmt.Printf("task1p: %T %+v %v\n", task1p, *task1p, unsafe.Sizeof(task1p)) // %Tで型取得、変数の頭に * をつけてポインタが示すアドレスに格納されている値を取得（この場合は直前で初期化された構造体）
+
+	(*task1p).Title = "Changed" // デリファレンスで構造体にアクセスして値を書き換えることが可能
+	fmt.Printf("task1p: %+v\n", *task1p)
+
+	task1.extendEstimate()
+	fmt.Printf("task1 value receiver: %+v\n", task1.Estimate)
+
+	task1.extendEstimatePointer()
+	fmt.Printf("task1 pointer receiver: %+v\n", task1.Estimate) // ポインタレシーバーを受け取る関数の処理では実体の値が変わっている
+}
+
+// receiver
+// 値レシーバーを持つ関数
+// Goではある型の実体に対する関数を定義できる
+// そのメソッドが受け取る型の実体をレシーバーと呼ぶ
+// 値レシーバーを受け取る関数で実体の値を変えても、実際は受け取ったレシーバーのコピーに対して変更を加えるため、実体には影響がない
+func (task Task) extendEstimate() {
+	task.Estimate += 10
+}
+
+// 実体に変更を加えたい場合はポインタレシーバーを使用する
+func (taskp *Task) extendEstimatePointer() {
+	taskp.Estimate += 10 // デリファレンスで実体にアクセスして、Estimateの値を変更している (*taskp).Estimate
 }
